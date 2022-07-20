b5db208989f0895254c1a889e31c36211f8da0c0
Karen Cueva <kjcueva1@espe.edu.ec>
2022-07-19 20:44:10 -0500

    Creacion de archivos

---
+++ py.py
@@ -0,0 +1,31 @@
+#Import libraries
+import os
+from flask import Flask, render_template
+from pyquery import PyQuery as pq
+
+
+app = Flask(__name__)
+app.secret_key = "LC"
+app.debug = False
+app._static_folder = os.path.abspath("templates/static/")
+
+#Funcion controladora para mostrar la página principal
+#Definicion de la ruta por defecto
+@app.route("/", methods=["GET"])
+#Llamar a index
+def index():
+    """
+        Creates the index page with all of its attributes.
+        Parameters
+        ----------
+        None
+        Returns
+        -------
+        The index page rendered
+    """
+    # Creates the input image
+    return render_template("/layouts/index.html")
+
+#Funcion main del programa
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
