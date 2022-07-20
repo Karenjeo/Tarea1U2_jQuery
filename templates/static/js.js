72c75ad8a078240e6d41154537eb669c2358f120
Karen Cueva <kjcueva1@espe.edu.ec>
2022-07-19 22:54:58 -0500

    Validaciones

--- templates/static/js.js
+++ templates/static/js.js
@@ -1,36 +1,72 @@
 $(document).ready(function() {
-    $("#basic-form").validate();
-    rules: {
-        nombre: {
-            required: true,
-            minlength: 3
-        },
-        apellido: {
-            required: true
-        },
-        email: {
-            required: true,
-            email: true
-        },
-        asunto: {
-            required: true,
-            minlength: 2
-        },
-        mensaje:{
-            required: true,
-            maxlength: 100
-        },
-        captcha: {
-            required: true
-        }
-    },
-  });
-function miFuncion() {
-    var response = grecaptcha.getResponse();
+    $('#basic-form').validate({
+        /**Reglas de validación */
+        rules:{
+            
+            //Se requiere que ingrese el nombre y tengan una logitud de al menos 3 caracteres
+            fname: {
+                required:true,
+                minlength:3
+            },
 
-    if(response.length == 0){
-      alert("Captcha no verificado")
-    } else {
-      alert("Captcha verificado");
-    }
-  }
+            lname: {
+                required:true,
+                minlength:3
+            },
+             //Se requiere que ingrese el correo electrónico
+            email: {
+                required:true
+            },
+            //Se requiere que ingrese el subject y tengan una logitud de al menos 10 caracteres
+            age:{
+                required:true,
+                minlength:2
+            },
+            //Se requiere que ingrese el mensaje y tengan una logitud de al menos 10 caracteres
+            mensaje:{
+                required:true,
+                minlength:100
+            },
+            
+            //Se requiere que ingrese el captcha y tengan una logitud de al menos 2 caracteres
+            captcha:{
+                required:true,
+                minlength:2
+            },
+        },
+            //mensajes de las reglas de validación
+            messages:{
+                nombre:{
+                    required:"Ingrese su nombre",
+                    minlength:"Debe tener al menos 3 caracteres"
+
+                }
+                ,
+                email:{
+                    required:"Ingrese su correo electronico"
+                },
+                subject:{
+                    required:"Ingrese el subject o tema",
+                    minlength:"Debe tener al menos 10 caracteres"
+                },
+                mensaje:{
+                    required:"Ingrese el mensaje",
+                    minlength:"Debe tener al menos 10 caracteres"
+                },
+                captcha:{
+                    required:"Ingrese el codigo",
+                    minlength:"Debe tener al menos 2 caracteres"
+                }
+
+            },
+            //envío de formulario
+            submitHandler: function(form) {
+
+                form.submit();
+          
+              }
+        
+
+    });
+    
+    });
